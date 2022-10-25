fd3f713cb848d854bf052afdd366a3da475b3509
Carleton Rose <>
2022-10-25 11:55:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print('''  
+  Ian
+  October 25th 2022
+  I am signing up for Replits 100 days of python challenge!
+  I will make sure to spend my time every day coding along, for a minimum 
+  of 10 minutes a day.
+  Ill be using replit , an amazing online IDE so i can do this from my 
+  phone wherever i happen to be.No excuses for not coding from the middle 
+  field!
+ ''')
